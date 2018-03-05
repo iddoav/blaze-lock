ca8af8d0c5ea4bfaa4d5e5eed30887b15d769856
iddoaviram <iddo.aviram@similarweb.com>
2018-03-05 12:37:03 +0200

    Revert fork-specific changes

--- setup.py
+++ setup.py
@@ -1,23 +1,24 @@
 from blazelock import __version__
 from distutils.core import setup
 # from setuptools import setup
-import setuptools
 
 VERSION = '0.1'
 
 setup(
-      name='sw_blazelock',
-      version=__version__,
-      description='Distributed Reader-Writer Lock using Redis - A SimilarWeb fork of blazelock by Kunal Lillaney'
-                  '(see https://github.io/kunallillaney/blazelock)',
-      license='Apache2.0',
-      packages=[
+    name='blazelock',
+    version=__version__,
+    description='Distributed Reader-Writer Lock using Redis',
+    author='Kunal Lillaney',
+    author_email='lillaney@jhu.edu',
+    url='https://github.io/kunallillaney/blazelock',
+    license='Apache2.0',
+    packages=[
         'blazelock'
-      ],
-      # setup_requires=[
-      # ],
-      install_requires=[
+    ],
+    # setup_requires=[
+    # ],
+    install_requires=[
         'redis>=2.10.5',
         'hiredis>=0.2.0'
-      ]
+    ]
 )
